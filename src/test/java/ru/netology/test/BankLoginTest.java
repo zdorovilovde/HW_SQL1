b5c14f9dba8785560fba9311422ffe4f4989a21b
zdorovilov test <zdorovilovde@mail.ru>
2024-09-05 23:59:55 +0500

    Update BankLoginTest

--- src/test/java/ru/netology/test/BankLoginTest.java
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -1,4 +1,31 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import java.nio.channels.ConnectionPendingException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
 public class BankLoginTest {
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to Dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999/", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
 }
