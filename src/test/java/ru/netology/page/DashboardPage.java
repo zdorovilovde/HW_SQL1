65e3c1f36dac89b896ecd913ccdedaf458dfe417
zdorovilov test <zdorovilovde@mail.ru>
2024-09-05 23:17:08 +0500

    Update DashboardPage

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,4 +1,14 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+
+    private DashboardPage() {
+        heading.shouldBe(Condition.visible);
+    }
 }
