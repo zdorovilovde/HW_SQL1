6521d7f961565a20b06bc4c6192403484a0c2800
zdorovilov test <zdorovilovde@mail.ru>
2024-09-05 23:20:46 +0500

    Update LoginPage

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,4 +1,29 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.page;
+
 public class LoginPage {
+    @FindBy(css = "[data-test-id=login] input")
+    private SelenideElement loginField;
+    @FindBy(css = "[data-test-id=password] input")
+    private SelenideElement passwordField;
+    @FindBy(css = "[data-test-id=action-login]")
+    private SelenideElement loginButton;
+    @FindBy(css = "[data-test-id='error-notification']")
+    private SelenideElement errorNotification;
+
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(Condition.visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return page(VerificationPage.class);
+    }
 }
