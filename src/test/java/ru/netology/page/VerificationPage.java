d7cff9a42251e47aa5ef4d72f9a91b87dc1f953e
zdorovilov test <zdorovilovde@mail.ru>
2024-09-05 23:37:36 +0500

    Update VerificationPage

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,4 +1,30 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.page;
+
 public class VerificationPage {
+    @FindBy(css = "[data-test-id=code] input")
+    private SelenideElement codeField;
+    @FindBy(css = "[data-test-id=action-verify]")
+    private SelenideElement verifyButton;
+    @FindBy(css = "[data-test-id='error-notification']")
+    private SelenideElement errorNotification;
+    public void verifyVerificationPageVisibility() {
+        codeField.shouldBe(Condition.visible);
+    }
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(Condition.visible);
+    }
+    public DashboardPage validVerify(String verificationCode){
+        verify(verificationCode);
+        return page(DashboardPage.class);
+    }
+    public void verify(String  verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
 }
