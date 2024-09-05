15adda63aed5da9ad6b98c401c890fc9b1662ca0
zdorovilov test <zdorovilovde@mail.ru>
2024-09-05 23:44:39 +0500

    Update LoginPage

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -3,6 +3,7 @@ package ru.netology.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.page;
 
